9330e319ce700a4f42075e2469d2dc38452b13b5
gnuxie <Gnuxie@protonmail.com>
2023-01-02 16:54:50 +0000

    Add new header to all files.

--- src/commands/MakeRoomAdminCommand.ts
+++ src/commands/MakeRoomAdminCommand.ts
@@ -1,4 +1,12 @@
-/*
+/**
+ * Copyright (C) 2022 Gnuxie <Gnuxie@protonmail.com>
+ * All rights reserved.
+ *
+ * This file is modified and is NOT licensed under the Apache License.
+ * This modified file incorperates work from mjolnir
+ * https://github.com/matrix-org/mjolnir
+ * which included the following license notice:
+
 Copyright 2021, 2022 Marco Cirillo
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +20,10 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-*/
+ *
+ * However, this file is modified and the modifications in this file
+ * are NOT distributed, contributed, committed, or licensed under the Apache License.
+ */
 
 import { Mjolnir } from "../Mjolnir";
 import { RichReply } from "matrix-bot-sdk";
