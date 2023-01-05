8184aeb45f522e552ca801a9c6456cad4cc634b5
David Teller <d.o.teller+github@gmail.com>
2023-01-05 11:12:48 +0100

    A first implementation of report-to-moderator
    Traditionally, when a user clicks "report" in a Matrix client, this goes
    to the homeserver administrator, who often is the wrong person for the job.
    
    MSC3215 introduces a mechanism to let clients cooperate with a bot to send
    the report to the moderator instead. Client support has landed in Element Web
    (behind a Labs flag) in in 2021. This allows Mjölnir to serve as the partner
    bot.

--- src/Mjolnir.ts
+++ src/Mjolnir.ts
@@ -129,7 +129,7 @@ export class Mjolnir {
             if (options.autojoinOnlyIfManager) {
                 const managers = await client.getJoinedRoomMembers(mjolnir.managementRoomId);
                 if (!managers.includes(membershipEvent.sender)) return reportInvite(); // ignore invite
-            } else {
+            } else if (options.acceptInvitesFromSpace) {
                 const spaceId = await client.resolveRoom(options.acceptInvitesFromSpace);
                 const spaceUserIds = await client.getJoinedRoomMembers(spaceId)
                     .catch(async e => {
@@ -154,7 +154,7 @@ export class Mjolnir {
      */
     static async setupMjolnirFromConfig(client: MatrixSendClient, matrixEmitter: MatrixEmitter, config: IConfig): Promise<Mjolnir> {
         if (!config.autojoinOnlyIfManager && config.acceptInvitesFromSpace === getDefaultConfig().acceptInvitesFromSpace) {
-            throw new TypeError("`autojoinOnlyIfManager` has been disabled, yet no space has been provided for `acceptInvitesFromSpace`.");
+            throw new TypeError("`autojoinOnlyIfManager` has been disabled but you have not set `acceptInvitesFromSpace`. Please make it empty to accept invites from everywhere or give it a namespace alias or room id.");
         }
         const policyLists: PolicyList[] = [];
         const joinedRooms = await client.getJoinedRooms();
