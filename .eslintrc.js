fdb6fdd7a8e0bf2d8caf720adbfc18af91834be4
MTRNord <mtrnord1@gmail.com>
2023-09-01 21:20:36 +0200

    Cleanup the eslint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
     "env": {
-        "browser": true,
+        "browser": false,
         "es6": true,
         "node": true
     },
@@ -14,86 +14,36 @@ module.exports = {
     ],
     "root": true,
     "rules": {
-        "@typescript-eslint/dot-notation": "off",
-        "@typescript-eslint/explicit-member-accessibility": [
-            "off",
+        "@typescript-eslint/member-ordering": [
+            "error",
             {
-                "accessibility": "explicit"
+                "default": [
+                    "public-instance-method",
+                    "public-static-field"
+                ]
             }
         ],
-        "@typescript-eslint/indent": "off",
-        "@typescript-eslint/member-delimiter-style": [
-            "off",
-            {
-                "multiline": {
-                    "delimiter": "none",
-                    "requireLast": true
-                },
-                "singleline": {
-                    "delimiter": "semi",
-                    "requireLast": false
-                }
-            }
-        ],
-        "@typescript-eslint/member-ordering": ["error", { "default": ["public-instance-method", "public-static-field"] }],
-        "@typescript-eslint/naming-convention": [
-            "off",
-            {
-                "selector": "variable",
-                "format": [
-                    "camelCase",
-                    "UPPER_CASE"
-                ],
-                "leadingUnderscore": "forbid",
-                "trailingUnderscore": "forbid"
-            }
-        ],
-        "@typescript-eslint/no-empty-function": "off",
         "@typescript-eslint/no-inferrable-types": "error",
         "@typescript-eslint/no-shadow": "error",
         "@typescript-eslint/no-unused-expressions": "error",
-        "@typescript-eslint/no-use-before-define": "off",
-        "@typescript-eslint/quotes": "off",
-        "@typescript-eslint/semi": [
-            "off",
-            null
-        ],
         "@typescript-eslint/type-annotation-spacing": "error",
         "brace-style": [
             "error",
             "1tbs"
         ],
-        "curly": "off",
-        "dot-notation": "off",
-        "eol-last": "off",
         "eqeqeq": [
             "error",
             "always"
         ],
-        "guard-for-in": "off",
-        "id-denylist": "off",
-        "id-match": "off",
-        "indent": "off",
-        "max-len": "off",
-        "no-bitwise": "off",
         "no-caller": "error",
-        "no-console": "off",
         "no-debugger": "error",
-        "no-empty": "off",
-        "no-empty-function": "off",
         "no-eval": "error",
         "no-fallthrough": "error",
         "no-new-wrappers": "error",
         "no-redeclare": "error",
-        "no-shadow": "off",
         "no-trailing-spaces": "error",
-        "no-underscore-dangle": "off",
-        "no-unused-expressions": "off",
         "no-unused-labels": "error",
-        "no-use-before-define": "off",
         "no-var": "error",
-        "quotes": "off",
-        "radix": "error",
-        "semi": "off"
+        "radix": "error"
     }
 };
