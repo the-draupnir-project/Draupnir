0204fb51398497b9dacc1c46926f2fab9a5cb2c7
MTRNord <mtrnord1@gmail.com>
2023-09-03 16:17:41 +0200

    Return the trace id instead of a random UUID if there is one available

--- src/commands/interface-manager/CommandException.ts
+++ src/commands/interface-manager/CommandException.ts
@@ -2,15 +2,15 @@
  * Copyright (C) 2023 Gnuxie <Gnuxie@protonmail.com>
  * All rights reserved.
  */
-
 import { randomUUID } from "crypto";
+import * as api from '@opentelemetry/api';
 import { CommandError, CommandResult } from "./Validation";
 import { trace } from "../../utils";
 
 // FIXME: I wonder if we could allow message to be JSX?
 //        Then room references could be put into the DM and actually mean something.
 export class CommandException extends CommandError {
-    public readonly uuid = randomUUID();
+    public readonly uuid: string = api.trace.getSpan(api.context.active())?.spanContext().traceId ?? randomUUID();
 
     constructor(
         public readonly exception: Error | unknown,
