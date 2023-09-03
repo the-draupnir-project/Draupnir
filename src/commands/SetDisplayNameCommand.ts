0daf94309a2edf181c6cdeddd96b525e0d4acd95
MTRNord <mtrnord1@gmail.com>
2023-09-03 15:37:43 +0200

    Add missing matrix interface adaptor and use CommandError

--- src/commands/SetDisplayNameCommand.ts
+++ src/commands/SetDisplayNameCommand.ts
@@ -1,8 +1,9 @@
-import { LogLevel, LogService } from "matrix-bot-sdk";
-import { defineInterfaceCommand } from "./interface-manager/InterfaceCommand";
+import { defineInterfaceCommand, findTableCommand } from "./interface-manager/InterfaceCommand";
 import { ParsedKeywords, findPresentationType, parameters } from "./interface-manager/ParameterParsing";
 import { MjolnirContext } from "./CommandHandler";
-import { CommandResult } from "./interface-manager/Validation";
+import { CommandError, CommandResult } from "./interface-manager/Validation";
+import { tickCrossRenderer } from "./interface-manager/MatrixHelpRenderer";
+import { defineMatrixInterfaceAdaptor } from "./interface-manager/MatrixInterfaceAdaptor";
 
 
 defineInterfaceCommand({
@@ -21,13 +22,19 @@ defineInterfaceCommand({
 
 // !draupnir displayname <displayname>
 export async function execSetDisplayNameCommand(this: MjolnirContext, _keywords: ParsedKeywords, displayname: string): Promise<CommandResult<any>> {
+
     try {
         await this.client.setDisplayName(displayname);
     } catch (e) {
         const message = e.message || (e.body ? e.body.error : '<no message>');
-        LogService.error("SetDisplayNameCommand", e);
-        await this.mjolnir.managementRoomOutput.logMessage(LogLevel.ERROR, "SetDisplayNameCommand", `Failed to set displayname to ${displayname}: ${message}`);
+        return CommandError.Result(`Failed to set displayname to ${displayname}: ${message}`)
     }
+
 
     return CommandResult.Ok(undefined);
 }
+
+defineMatrixInterfaceAdaptor({
+    interfaceCommand: findTableCommand("mjolnir", "displayname"),
+    renderer: tickCrossRenderer
+})
