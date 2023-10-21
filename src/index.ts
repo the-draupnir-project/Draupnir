5738d764d4623496543e13526e336dc7fe1b49e6
Marcel <MTRNord@users.noreply.github.com>
2023-10-21 13:18:10 +0200

    Add support for bot-sdk crypto as an experimental flag (#145)
    * Add support for bot-sdk crypto as an experimental flag
    
    * Import and use StoreType enum
    
    * Fail hard instead of continuing in case of having pantalaimon and bot-sdk set up
    
    * Document Option in default.yaml
    
    * Remove obsolete comment

--- src/index.ts
+++ src/index.ts
@@ -35,9 +35,10 @@ import {
     MatrixClient,
     PantalaimonClient,
     RichConsoleLogger,
-    SimpleFsStorageProvider
+    SimpleFsStorageProvider,
+    RustSdkCryptoStorageProvider
 } from "matrix-bot-sdk";
-
+import { StoreType } from "@matrix-org/matrix-sdk-crypto-nodejs";
 import { read as configRead } from "./config";
 import { Mjolnir } from "./Mjolnir";
 import { initializeSentry, patchMatrixClient } from "./utils";
@@ -69,9 +70,16 @@ import { initializeSentry, patchMatrixClient } from "./utils";
         const storage = new SimpleFsStorageProvider(path.join(storagePath, "bot.json"));
 
         let client: MatrixClient;
-        if (config.pantalaimon.use) {
+        if (config.pantalaimon.use && !config.experimentalRustCrypto) {
             const pantalaimon = new PantalaimonClient(config.homeserverUrl, storage);
             client = await pantalaimon.createClientWithCredentials(config.pantalaimon.username, config.pantalaimon.password);
+        } else if (config.experimentalRustCrypto) {
+            if (config.pantalaimon.use) {
+                throw Error("You have a pantalaimon config activated and experimentalRustCrypto. Make sure the accessToken is set and pantalaimon is disabled!");
+            }
+            const cryptoStorage = new RustSdkCryptoStorageProvider(path.join(storagePath, "crypto"), StoreType.Sqlite);
+
+            client = new MatrixClient(config.homeserverUrl, config.accessToken, storage, cryptoStorage);
         } else {
             client = new MatrixClient(config.homeserverUrl, config.accessToken, storage);
         }
