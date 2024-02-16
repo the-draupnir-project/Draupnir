57fa435bbcc757598335faf55d25fabdc58c61c0
gnuxie <Gnuxie@protonmail.com>
2024-02-16 17:19:20 +0000

    Load Draupnir protections in the DraupnirProtectedRoomsSet.

---
+++ src/protections/DraupnirProtectionsIndex.ts
@@ -0,0 +1,15 @@
+/**
+ * This file exists as a way to register all protections.
+ * In future, we should maybe try to dogfood the dynamic plugin load sytem
+ * instead. For now that system doesn't even exist.
+ */
+
+// keep alphabetical please.
+import './BanPropagation';
+import './BasicFlooding';
+import './FirstMessageIsImage';
+import './JoinWaveShortCircuit';
+import './MessageIsMedia';
+import './MessageIsVoice';
+import './TrustedReporters';
+import './WordList';
