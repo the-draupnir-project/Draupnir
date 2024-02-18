1925a51dc2e2c4fb6d4d6061bb401e00a0de0648
gnuxie <Gnuxie@protonmail.com>
2024-02-18 17:07:37 +0000

    Naively fix appservice integration tests.

--- test/appservice/utils/harness.ts
+++ test/appservice/utils/harness.ts
@@ -3,8 +3,8 @@ import { MjolnirAppService } from "../../../src/appservice/AppService";
 import { ensureAliasedRoomExists } from "../../integration/mjolnirSetupUtils";
 import { read as configRead, IConfig } from "../../../src/appservice/config/config";
 import { newTestUser } from "../../integration/clientHelper";
-import PolicyList from "../../../src/models/PolicyList";
 import { CreateEvent, MatrixClient } from "matrix-bot-sdk";
+import { POLICY_ROOM_TYPE_VARIANTS } from "matrix-protection-suite";
 
 export function readTestConfig(): IConfig {
     return configRead(path.join(__dirname, "../../../src/appservice/config/config.harness.yaml"));
@@ -19,5 +19,5 @@ export async function setupHarness(): Promise<MjolnirAppService> {
 
 export async function isPolicyRoom(user: MatrixClient, roomId: string): Promise<boolean> {
     const createEvent = new CreateEvent(await user.getRoomStateEvent(roomId, "m.room.create", ""));
-    return PolicyList.ROOM_TYPE_VARIANTS.includes(createEvent.type);
+    return POLICY_ROOM_TYPE_VARIANTS.includes(createEvent.type);
 }
