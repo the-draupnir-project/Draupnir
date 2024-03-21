c9db03304310256e912e5892dc94d08a2923a707
gnuxie <Gnuxie@protonmail.com>
2024-03-21 16:51:50 +0000

    fix utilsTest.ts

--- test/integration/utilsTest.ts
+++ test/integration/utilsTest.ts
@@ -1,33 +1,34 @@
 import { strict as assert } from "assert";
 import { LogLevel } from "matrix-bot-sdk";
-import ManagementRoomOutput from "../../src/ManagementRoomOutput";
+import { DraupnirTestContext, draupnirClient } from "./mjolnirSetupUtils";
 
 describe("Test: utils", function() {
-    it("replaceRoomIdsWithPills correctly turns a room ID in to a pill", async function() {
+    it("replaceRoomIdsWithPills correctly turns a room ID in to a pill", async function(this: DraupnirTestContext) {
         const managementRoomAlias = this.config.managementRoom;
-        const managementRoomOutput: ManagementRoomOutput = this.mjolnir.managementRoomOutput;
-        await this.mjolnir.client.sendStateEvent(
-            this.mjolnir.managementRoomId,
+        const draupnir = this.draupnir!;
+        const managementRoomOutput = draupnir.managementRoomOutput;
+        await draupnir.client.sendStateEvent(
+            draupnir.managementRoomID,
             "m.room.canonical_alias",
             "",
             { alias: managementRoomAlias }
         );
 
         const message: any = await new Promise(async resolve => {
-            this.mjolnir.client.on('room.message', (roomId, event) => {
-                if (roomId === this.mjolnir.managementRoomId) {
+            draupnirClient()!.on('room.message', (roomId, event) => {
+                if (roomId === draupnir.managementRoomID) {
                     if (event.content?.body?.startsWith("it's")) {
                         resolve(event);
                     }
                 }
             })
             await managementRoomOutput.logMessage(LogLevel.INFO, 'replaceRoomIdsWithPills test',
-                `it's fun here in ${this.mjolnir.managementRoomId}`,
-                [this.mjolnir.managementRoomId, "!myfaketestid:example.com"]);
+                `it's fun here in ${draupnir.managementRoomID}`,
+                [draupnir.managementRoomID, "!myfaketestid:example.com"]);
         });
         assert.equal(
             message.content.formatted_body,
             `it's fun here in <a href="https://matrix.to/#/${encodeURIComponent(managementRoomAlias)}">${managementRoomAlias}</a>`
         );
-    });
+    } as unknown as Mocha.AsyncFunc);
 });
