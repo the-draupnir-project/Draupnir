982f9c23c88bcfa76df6fe754ee4ff2a5bd5ce98
Gnuxie <50846879+Gnuxie@users.noreply.github.com>
2024-09-09 18:08:14 +0100

    Give MatrixInterfaceAdaptor all of its required callbacks. (#540)
    * Update to @the-draupnir-project/interface-manager@2.0.0.
    
    The interface has been changed in `interface-manager` so that we can't forget them. 
    
    Means that our little error renderer works again.

--- src/commands/DraupnirCommandPrerequisites.ts
+++ src/commands/DraupnirCommandPrerequisites.ts
@@ -13,16 +13,11 @@ import {
 } from "@the-draupnir-project/interface-manager";
 import { Draupnir } from "../Draupnir";
 import {
+  MPSCommandDispatcherCallbacks,
+  MPSMatrixInterfaceAdaptorCallbacks,
   MatrixEventContext,
   invocationInformationFromMatrixEventcontext,
-  matrixEventsFromDeadDocument,
-  rendererFailedCB,
 } from "./interface-manager/MPSMatrixInterfaceAdaptor";
-import { matrixCommandRenderer } from "./interface-manager/MatrixHelpRenderer";
-import {
-  promptDefault,
-  promptSuggestions,
-} from "./interface-manager/MatrixPromptForAccept";
 
 export const DraupnirContextToCommandContextTranslator =
   new StandardAdaptorContextToCommandContextTranslator<Draupnir>();
@@ -32,9 +27,6 @@ export const DraupnirInterfaceAdaptor = new StandardMatrixInterfaceAdaptor<
 >(
   DraupnirContextToCommandContextTranslator,
   invocationInformationFromMatrixEventcontext,
-  promptDefault,
-  promptSuggestions,
-  matrixCommandRenderer,
-  matrixEventsFromDeadDocument,
-  rendererFailedCB
+  MPSMatrixInterfaceAdaptorCallbacks,
+  MPSCommandDispatcherCallbacks
 );
