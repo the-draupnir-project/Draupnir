8d1af843d389b3ec1299f8477370e9bd02a3fce2
Gnuxie <50846879+Gnuxie@users.noreply.github.com>
2024-09-09 17:20:09 +0100

    Unit test the DraupnirKickCommand. (#539)
    * Update to MPS v1.2.1.
    
    This will allow us to use `describeProtectedRoomsSet` in unit tests.
    
    * Update to @the-draupnir-project/interface-manager@1.1.1.
    
    This allows for better type inference from the command and also for
    partial keywords to be provided to commands as arguments.
    
    * Unit test the DraupnirKickCommand.

---
+++ test/unit/commands/KickCommandTest.ts
@@ -0,0 +1,142 @@
+// SPDX-FileCopyrightText: 2024 Gnuxie <Gnuxie@protonmail.com>
+//
+// SPDX-License-Identifier: AFL-3.0
+
+import { CommandExecutorHelper } from "@the-draupnir-project/interface-manager";
+import {
+  MatrixRoomID,
+  MatrixUserID,
+  StringUserID,
+  userServerName,
+} from "@the-draupnir-project/matrix-basic-types";
+import {
+  Membership,
+  Ok,
+  RoomKicker,
+  RoomResolver,
+  describeProtectedRoomsSet,
+  isError,
+} from "matrix-protection-suite";
+import { DraupnirKickCommand } from "../../../src/commands/KickCommand";
+import { ThrottlingQueue } from "../../../src/queues/ThrottlingQueue";
+import ManagementRoomOutput from "../../../src/ManagementRoomOutput";
+import { createMock } from "ts-auto-mock";
+import expect from "expect";
+
+async function createProtectedRooms() {
+  return await describeProtectedRoomsSet({
+    rooms: [
+      {
+        membershipDescriptions: [...Array(50)].map((_, index) => ({
+          membership: Membership.Join,
+          sender: `@${index}:testserver.example.com` as StringUserID,
+        })),
+      },
+      {
+        membershipDescriptions: [
+          {
+            membership: Membership.Join,
+            sender: `@alice:testserver.example.com` as StringUserID,
+          },
+          {
+            membership: Membership.Join,
+            sender: `@bob:bob.example.com` as StringUserID,
+          },
+        ],
+      },
+    ],
+  });
+}
+
+const taskQueue = new ThrottlingQueue(createMock<ManagementRoomOutput>(), 0);
+
+describe("Test the KickCommand", function () {
+  const roomResolver = createMock<RoomResolver>({
+    async resolveRoom(roomReference) {
+      if (roomReference instanceof MatrixRoomID) {
+        return Ok(roomReference);
+      }
+      throw new TypeError(`We don't really expect to resolve anything`);
+    },
+  });
+  it("Will kick users from protected rooms when a glob is used", async function () {
+    const { protectedRoomsSet } = await createProtectedRooms();
+    const roomKicker = createMock<RoomKicker>({
+      async kickUser(_room, userID, _reason) {
+        // We should only kick users that match the glob...
+        expect(userServerName(userID)).toBe("testserver.example.com");
+        return Ok(undefined);
+      },
+    });
+    const kickResult = await CommandExecutorHelper.execute(
+      DraupnirKickCommand,
+      {
+        taskQueue,
+        setMembership: protectedRoomsSet.setMembership,
+        roomKicker,
+        roomResolver,
+        noop: false,
+      },
+      {
+        keywords: { glob: true },
+      },
+      MatrixUserID.fromUserID(`@*:testserver.example.com` as StringUserID)
+    );
+    if (isError(kickResult)) {
+      throw new TypeError(`We don't expect the kick command itself to fail`);
+    }
+    const usersToKick = kickResult.ok;
+    expect(usersToKick.size).toBe(51);
+  });
+  it("Will refuse to kick anyone with a glob if the glob flag is not set", async function () {
+    const { protectedRoomsSet } = await createProtectedRooms();
+    const kickResult = await CommandExecutorHelper.execute(
+      DraupnirKickCommand,
+      {
+        taskQueue,
+        setMembership: protectedRoomsSet.setMembership,
+        roomKicker: createMock<RoomKicker>(),
+        roomResolver,
+        noop: false,
+      },
+      {},
+      MatrixUserID.fromUserID(`@*:testserver.example.com` as StringUserID)
+    );
+    expect(isError(kickResult)).toBe(true);
+  });
+  it("Will limit the scope to one room if the --room option is provided", async function () {
+    const { protectedRoomsSet } = await createProtectedRooms();
+    const targetRoom = protectedRoomsSet.allProtectedRooms[1];
+    if (targetRoom === undefined) {
+      throw new TypeError(`Something is wrong with the test!!`);
+    }
+    const roomKicker = createMock<RoomKicker>({
+      async kickUser(room, userID, _reason) {
+        // We should only kick users that match the glob...
+        expect(userServerName(userID)).toBe("testserver.example.com");
+        // We should only kick users in the target room.
+        expect(room.toString()).toBe(targetRoom.toRoomIDOrAlias());
+        return Ok(undefined);
+      },
+    });
+    const kickResult = await CommandExecutorHelper.execute(
+      DraupnirKickCommand,
+      {
+        taskQueue,
+        setMembership: protectedRoomsSet.setMembership,
+        roomKicker,
+        roomResolver,
+        noop: false,
+      },
+      {
+        keywords: { glob: true, room: targetRoom },
+      },
+      MatrixUserID.fromUserID(`@*:testserver.example.com` as StringUserID)
+    );
+    if (isError(kickResult)) {
+      throw new TypeError(`We don't expect the kick command itself to fail`);
+    }
+    const usersToKick = kickResult.ok;
+    expect(usersToKick.size).toBe(1);
+  });
+});
