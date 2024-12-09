46afeb9cb8528036f2ccf35773498d9b1ab00756
gnuxie <Gnuxie@protonmail.com>
2024-12-09 16:16:29 +0000

    Enable standard presentation type translators.
    This will fix issues with the ban command reason.

--- src/safemode/commands/SafeModeCommands.tsx
+++ src/safemode/commands/SafeModeCommands.tsx
@@ -2,13 +2,25 @@
 //
 // SPDX-License-Identifier: AFL-3.0
 
-import { StandardCommandTable } from "@the-draupnir-project/interface-manager";
+import {
+  StandardCommandTable,
+  StringFromMatrixEventReferenceTranslator,
+  StringFromMatrixRoomAliasTranslator,
+  StringFromMatrixRoomIDTranslator,
+  StringFromMatrixUserIDTranslator,
+  StringFromNumberTranslator,
+} from "@the-draupnir-project/interface-manager";
 import { SafeModeHelpCommand } from "./HelpCommand";
 import { SafeModeStatusCommand } from "./StatusCommand";
 import { SafeModeRestartCommand } from "./RestartDraupnirCommand";
 import { SafeModeRecoverCommand } from "./RecoverCommand";
 
 export const SafeModeCommands = new StandardCommandTable("safe mode")
+  .internPresentationTypeTranslator(StringFromNumberTranslator)
+  .internPresentationTypeTranslator(StringFromMatrixRoomIDTranslator)
+  .internPresentationTypeTranslator(StringFromMatrixRoomAliasTranslator)
+  .internPresentationTypeTranslator(StringFromMatrixUserIDTranslator)
+  .internPresentationTypeTranslator(StringFromMatrixEventReferenceTranslator)
   .internCommand(SafeModeHelpCommand, ["draupnir", "help"])
   .internCommand(SafeModeStatusCommand, ["draupnir", "status"])
   .internCommand(SafeModeRecoverCommand, ["draupnir", "recover"])
