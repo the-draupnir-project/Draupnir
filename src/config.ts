7b558b29ee1d2904465997bd6f7a52ac23285c3d
gnuxie <Gnuxie@protonmail.com>
2025-01-20 12:28:08 +0000

    Extract only the first line from secret files.

--- src/config.ts
+++ src/config.ts
@@ -352,14 +352,12 @@ export function configRead(): IConfig {
     "--pantalaimon-password-path"
   );
   if (explicitAccessTokenPath !== undefined) {
-    config.accessToken = fs
-      .readFileSync(explicitAccessTokenPath, "utf8")
-      .trim();
+    config.accessToken = readSecretFromPath(explicitAccessTokenPath);
   }
   if (explicitPantalaimonPasswordPath) {
-    config.pantalaimon.password = fs
-      .readFileSync(explicitPantalaimonPasswordPath, "utf8")
-      .trim();
+    config.pantalaimon.password = readSecretFromPath(
+      explicitPantalaimonPasswordPath
+    );
   }
   return config;
 }
@@ -483,6 +481,15 @@ function getCommandLineOption(
 
   // No value was provided, or the next argument is another option
   throw new Error(`No value provided for ${optionName}`);
+}
+
+function readSecretFromPath(path: string): string {
+  // extract only the first line.
+  const secret = fs.readFileSync(path, "utf8").match(/^[^\r\n]*/)?.[0];
+  if (!secret) {
+    throw new TypeError(`There is no secret present in the file at ${path}`);
+  }
+  return secret;
 }
 
 type UnknownPropertyPaths = string[];
