377207ce3f4e4b37656ce592a1d287abfcc80895
gnuxie <Gnuxie@protonmail.com>
2025-03-03 18:10:16 +0000

    Add even more test cases to unban test.

--- test/integration/commands/unbanTest.ts
+++ test/integration/commands/unbanTest.ts
@@ -343,4 +343,285 @@ describe("unbanCommandTest", function () {
       }
     } as unknown as Mocha.AsyncFunc
   );
+  it(
+    "unbans users when there is no membership and only policies",
+    async function (this: DraupnirTestContext) {
+      const draupnir = this.draupnir;
+      if (draupnir === undefined) {
+        throw new TypeError(`setup didn't run properly`);
+      }
+      const falsePositiveUser = await newTestUser(this.config.homeserverUrl, {
+        name: { contains: "accidentally-banned" },
+      });
+      const falsePositiveUserID =
+        (await falsePositiveUser.getUserId()) as StringUserID;
+      const policyRoomID = await createWatchedPolicyRoom(draupnir);
+      const policyRoomEditor = (
+        await draupnir.policyRoomManager.getPolicyRoomEditor(
+          MatrixRoomReference.fromRoomID(policyRoomID)
+        )
+      ).expect(
+        "Should be able to get a policy room editor for the newly created policy room"
+      );
+      await createPoliciesBanningUser(policyRoomEditor, falsePositiveUserID);
+      // wait for policies to be detected.
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      // Now we can use the unban command to test the preview has no effects
+      // So the way this can work is we can send the command, get back the event and just know that we can send 'OK' and 'Cancel' to it later and it'll work.
+      const previewResult = (
+        await draupnir.sendTextCommand(
+          draupnir.clientUserID,
+          `!draupnir unban ${falsePositiveUserID}`
+        )
+      ).expect(
+        "We should have been able to get a preview"
+      ) as UnbanMembersPreview;
+      expect(previewResult.membersToUnban.length).toBe(0);
+      expect(previewResult.policyMatchesToRemove.length).toBe(1);
+      const listMatches = previewResult.policyMatchesToRemove.at(0);
+      if (listMatches === undefined) {
+        throw new TypeError("We should have some matches");
+      }
+      expect(listMatches.matches.length).toBe(3);
+      (
+        await draupnir.sendTextCommand(
+          draupnir.clientUserID,
+          `!draupnir unban ${falsePositiveUserID} --no-confirm`
+        )
+      ).expect(
+        "We should have been able to run the command"
+      ) as UnbanMembersResult;
+      // wait for events to come down sync
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      // verify the policies are removed.
+      const policyRevision =
+        draupnir.protectedRoomsSet.watchedPolicyRooms.currentRevision;
+      expect(policyRevision.allRules().length).toBe(0);
+    } as unknown as Mocha.AsyncFunc
+  );
+  it(
+    "Unbans all members targetted by a glob that is being removed indirectly by unbanning a single user in the command",
+    async function (this: DraupnirTestContext) {
+      const draupnir = this.draupnir;
+      if (draupnir === undefined) {
+        throw new TypeError(`setup didn't run properly`);
+      }
+      const falsePositiveUser = await newTestUser(this.config.homeserverUrl, {
+        name: { contains: "banned-and-cool" },
+      });
+      const indirectlyBannedUser = await newTestUser(
+        this.config.homeserverUrl,
+        {
+          name: { contains: "indirectly-banned-via-glob-and-cool" },
+        }
+      );
+      const indirectButShouldBeBanned = await newTestUser(
+        this.config.homeserverUrl,
+        {
+          name: { contains: "indirectly-banned-but-should-be-banned-and-cool" },
+        }
+      );
+      const falsePositiveUserID =
+        (await falsePositiveUser.getUserId()) as StringUserID;
+      const indirectlyBannedUserID =
+        (await indirectlyBannedUser.getUserId()) as StringUserID;
+      const indirectButShouldBeBannedUserID =
+        (await indirectButShouldBeBanned.getUserId()) as StringUserID;
+      const protectedRooms = await createProtectedRoomsSetWithBan(
+        draupnir,
+        falsePositiveUserID,
+        { numberOfRooms: 5 }
+      );
+      for (const room of protectedRooms) {
+        (
+          await draupnir.clientPlatform
+            .toRoomBanner()
+            .banUser(room, indirectlyBannedUserID, "spam")
+        ).expect("Should be able to ban people");
+        (
+          await draupnir.clientPlatform
+            .toRoomBanner()
+            .banUser(room, indirectButShouldBeBannedUserID, "spam")
+        ).expect("should be able to ban people");
+      }
+      const policyRoomID = await createWatchedPolicyRoom(draupnir);
+      const policyRoomEditor = (
+        await draupnir.policyRoomManager.getPolicyRoomEditor(
+          MatrixRoomReference.fromRoomID(policyRoomID)
+        )
+      ).expect(
+        "Should be able to get a policy room editor for the newly created policy room"
+      );
+      (
+        await policyRoomEditor.createPolicy(
+          PolicyRuleType.User,
+          Recommendation.Ban,
+          indirectButShouldBeBannedUserID,
+          "spam",
+          {}
+        )
+      ).expect(
+        "Should be able to create a policy banning just the user we want to stay banned"
+      );
+      (
+        await policyRoomEditor.createPolicy(
+          PolicyRuleType.User,
+          Recommendation.Ban,
+          `@*and-cool*:*`,
+          "spam",
+          {}
+        )
+      ).expect(
+        "Should be able to create a glob policy that bans all our targets"
+      );
+      // wait for policies to be detected.
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      // verify policies exist
+      expect(
+        draupnir.protectedRoomsSet.watchedPolicyRooms.currentRevision.allRules()
+          .length
+      ).toBe(2);
+      // Now we can use the unban command to test the preview has no effects
+      // So the way this can work is we can send the command, get back the event and just know that we can send 'OK' and 'Cancel' to it later and it'll work.
+      const previewResult = (
+        await draupnir.sendTextCommand(
+          draupnir.clientUserID,
+          `!draupnir unban ${falsePositiveUserID}`
+        )
+      ).expect(
+        "We should have been able to get a preview"
+      ) as UnbanMembersPreview;
+      expect(previewResult.membersToUnban.length).toBe(2);
+      expect(previewResult.policyMatchesToRemove.length).toBe(1);
+      const listMatches = previewResult.policyMatchesToRemove.at(0);
+      if (listMatches === undefined) {
+        throw new TypeError("We should have some matches");
+      }
+      expect(listMatches.matches.length).toBe(1);
+      const falsePositiveMember = previewResult.membersToUnban.at(0);
+      if (falsePositiveMember === undefined) {
+        throw new TypeError("We should have some details here");
+      }
+      const indirectlyBannedMember = previewResult.membersToUnban.at(1);
+      if (indirectlyBannedMember === undefined) {
+        throw new TypeError("We should have some details here");
+      }
+      expect(indirectlyBannedMember.roomsBannedFrom.length).toBe(5);
+      expect(falsePositiveMember.member).toBe(falsePositiveUserID);
+      expect(indirectlyBannedMember.member).toBe(indirectlyBannedUserID);
+      // now checked that the user is still banned in all 5 rooms
+      for (const roomID of protectedRooms) {
+        const membershipRevision =
+          draupnir.protectedRoomsSet.setRoomMembership.getRevision(roomID);
+        if (membershipRevision === undefined) {
+          throw new TypeError(
+            "Unable to find membership revision for a protected room, shouldn't happen"
+          );
+        }
+        expect(
+          membershipRevision.membershipForUser(indirectButShouldBeBannedUserID)
+            ?.membership
+        ).toBe(Membership.Ban);
+      }
+      (
+        await draupnir.sendTextCommand(
+          draupnir.clientUserID,
+          `!draupnir unban ${falsePositiveUserID} --no-confirm`
+        )
+      ).expect(
+        "We should have been able to run the command"
+      ) as UnbanMembersResult;
+      // wait for events to come down sync
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      // now check that they are unbanned
+      for (const roomID of protectedRooms) {
+        const membershipRevision =
+          draupnir.protectedRoomsSet.setRoomMembership.getRevision(roomID);
+        if (membershipRevision === undefined) {
+          throw new TypeError(
+            "Unable to find membership revision for a protected room, shouldn't happen"
+          );
+        }
+        expect(
+          membershipRevision.membershipForUser(indirectlyBannedUserID)
+            ?.membership
+        ).toBe(Membership.Leave);
+        expect(
+          membershipRevision.membershipForUser(indirectButShouldBeBannedUserID)
+            ?.membership
+        ).toBe(Membership.Ban);
+      }
+      // verify the policies are removed.
+      const policyRevision =
+        draupnir.protectedRoomsSet.watchedPolicyRooms.currentRevision;
+      expect(policyRevision.allRules().length).toBe(1);
+    } as unknown as Mocha.AsyncFunc
+  );
+  it(
+    "removes glob rules when there are only glob rules and no banned members",
+    async function (this: DraupnirTestContext) {
+      const draupnir = this.draupnir;
+      if (draupnir === undefined) {
+        throw new TypeError(`setup didn't run properly`);
+      }
+      const policyRoomID = await createWatchedPolicyRoom(draupnir);
+      const policyRoomEditor = (
+        await draupnir.policyRoomManager.getPolicyRoomEditor(
+          MatrixRoomReference.fromRoomID(policyRoomID)
+        )
+      ).expect(
+        "Should be able to get a policy room editor for the newly created policy room"
+      );
+      const globRuleEntity = `@*glob*:*`;
+      (
+        await policyRoomEditor.createPolicy(
+          PolicyRuleType.User,
+          Recommendation.Ban,
+          globRuleEntity,
+          "spam",
+          {}
+        )
+      ).expect(
+        "Should be able to create a glob policy that bans all our targets"
+      );
+      // wait for policies to be detected.
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      expect(
+        draupnir.protectedRoomsSet.watchedPolicyRooms.currentRevision.allRules()
+          .length
+      ).toBe(1);
+      // Now we can use the unban command to test the preview has no effects
+      // So the way this can work is we can send the command, get back the event and just know that we can send 'OK' and 'Cancel' to it later and it'll work.
+      const previewResult = (
+        await draupnir.sendTextCommand(
+          draupnir.clientUserID,
+          `!draupnir unban ${globRuleEntity}`
+        )
+      ).expect(
+        "We should have been able to get a preview"
+      ) as UnbanMembersPreview;
+      expect(previewResult.membersToUnban.length).toBe(0);
+      expect(previewResult.policyMatchesToRemove.length).toBe(1);
+      const listMatches = previewResult.policyMatchesToRemove.at(0);
+      if (listMatches === undefined) {
+        throw new TypeError("We should have some matches");
+      }
+      expect(listMatches.matches.length).toBe(1);
+      (
+        await draupnir.sendTextCommand(
+          draupnir.clientUserID,
+          `!draupnir unban ${globRuleEntity} --no-confirm`
+        )
+      ).expect(
+        "We should have been able to run the command"
+      ) as UnbanMembersResult;
+      // wait for events to come down sync
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      // verify the policies are removed.
+      const policyRevision =
+        draupnir.protectedRoomsSet.watchedPolicyRooms.currentRevision;
+      expect(policyRevision.allRules().length).toBe(0);
+    } as unknown as Mocha.AsyncFunc
+  );
 });
