eb2dbaeece05b3156927605c22080058825e184a
Catalan Lover <catalanlover@protonmail.com>
2026-09-03 18:02:16 +0200

    Introduce Ci in a Box
    Ci in a box is the next generation Ci project for Draupnir aiming to kill our dependence on mx-tester in the long run as its unmaintained and less powerful than ci-in-a-box

---
+++ ci-in-a-box/scripts/postgres/init.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE "synapse-test-db"
+    WITH OWNER = "mjolnir-tester"
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    TEMPLATE = template0;
+    CREATE DATABASE "mjolnir-test-db"
+        WITH OWNER = "mjolnir-tester"
+        ENCODING = 'UTF8'
+        LC_COLLATE = 'C'
+        LC_CTYPE = 'C'
+        TEMPLATE = template0;
